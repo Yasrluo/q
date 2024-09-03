cd8f143d21c8492fdfb8eb7fda19551251bac530
Yasrluo <136265021+Yasrluo@users.noreply.github.com>
2024-09-03 20:48:55 +0300

    Update stop.py

--- BrandrdXMusic/plugins/admins/stop.py
+++ BrandrdXMusic/plugins/admins/stop.py
@@ -8,8 +8,9 @@ from BrandrdXMusic.utils.decorators import AdminRightsCheck
 from BrandrdXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
-@app.on_message(filters.command(["ايقاف","انهاء","قف"], "")
-)
+@app.on_message(
+filters.command(["ايقاف","قف","انهاء"], "")
+    & ~BANNED_USERS )
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
