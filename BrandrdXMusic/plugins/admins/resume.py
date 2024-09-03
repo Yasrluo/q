93774660012cc6d1c2afe2f2f5999942ef9a0c35
Yasrluo <136265021+Yasrluo@users.noreply.github.com>
2024-09-03 20:43:26 +0300

    Update resume.py

--- BrandrdXMusic/plugins/admins/resume.py
+++ BrandrdXMusic/plugins/admins/resume.py
@@ -8,7 +8,8 @@ from BrandrdXMusic.utils.decorators import AdminRightsCheck
 from BrandrdXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
-
+@app.on_message(filters.command(["اعاده"], "")
+)
 @app.on_message(filters.command(["resume", "cresume"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
